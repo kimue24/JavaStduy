f08fb751436c17f2ee1b758419891d7781a89c67
kimue24 <54491294+kimue24@users.noreply.github.com>
2020-08-28 17:54:24 +0900

    VER 1.3

---
+++ JavaStudy/src/PrintfEx1.java
@@ -0,0 +1,32 @@
+
+public class PrintfEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte  b = 1;
+		short s = 2;
+		char  c = 'A';
+		
+		int finger = 10;
+		long big = 100_000_000L; // long big = 100000000L;
+		long hex = 0xFFFF_FFFF_FFFF_FFFFL ;
+		
+		int octNum = 010; // 8진수 10, 10진수로는 8
+		int hexNum = 0x10; // 16진수 10, 10진수로는 16
+		int binNum = 0b10; // 2진수 10, 10진수로는 2
+		
+		System.out.printf("b=%d%n",b);
+		System.out.printf("s=%d%n", s);
+		System.out.printf("c=%c,%d%n",c, (int)c);
+		
+		
+		
+		
+		
+
+		
+		
+		
+	}
+
+}
