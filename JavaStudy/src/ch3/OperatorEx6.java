476e233d9052b3fb50c70fc52aec4ce82934188f
kimue24 <54491294+kimue24@users.noreply.github.com>
2020-11-17 10:07:01 +0900

    1117 자바공부

---
+++ JavaStudy/src/ch3/OperatorEx6.java
@@ -0,0 +1,13 @@
+package ch3;
+
+public class OperatorEx6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//byte a = 10;
+		//byte b = 30;
+		//byte c =  a + b; //컴파일 에러가 발생합니다. 형변환이 필요합니다.
+		//System.out.println(c);
+	}
+
+}
