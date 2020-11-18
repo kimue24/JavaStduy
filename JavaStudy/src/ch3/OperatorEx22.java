feb301efc1b8258d218d0138085af949075a81cf
kimue24 <54491294+kimue24@users.noreply.github.com>
2020-11-18 09:12:13 +0900

    1118 java study

--- JavaStudy/src/ch3/OperatorEx22.java
+++ JavaStudy/src/ch3/OperatorEx22.java
@@ -1,9 +1,10 @@
 package ch3;
 
-public class OperatorEx07 {
+public class OperatorEx22 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		float f = 0.1f;
 
 	}
 
