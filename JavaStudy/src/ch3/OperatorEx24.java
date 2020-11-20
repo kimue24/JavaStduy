78e2f1a63c88116fceb162c7ea0cc928df357d7b
kimue24 <54491294+kimue24@users.noreply.github.com>
2020-11-20 09:29:00 +0900

    1120

---
+++ JavaStudy/src/ch3/OperatorEx24.java
@@ -0,0 +1,25 @@
+package ch3;
+
+public class OperatorEx24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 0;
+		char ch = ' ';
+		
+		x = 15;
+		System.out.printf("x=%2d, 10 < x && x < 20 =%b%n", x, 10<x && x<20);
+		
+		x = 6;
+		System.out.printf("x=%2d, x%%2==0 || x%%3 ==0 && x%%6!=0 = %b%n", x, x%2==0|| x%3==0&&x%6!=0);
+
+		System.out.printf("x=%2d, x%%2==0 || x%%3 ==0 && x%%6!=0 = %b%n", x, (x%2==0|| x%3==0)&&x%6!=0);
+	
+		ch='1';
+
+	
+	
+	
+	}
+
+}
