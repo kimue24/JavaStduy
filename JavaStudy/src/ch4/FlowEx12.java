bebdbc589273391927eb5427c900b6e4cf173ba7
kimue24 <54491294+kimue24@users.noreply.github.com>
2020-12-30 10:21:29 +0900

    Create FlowEx12.java

---
+++ JavaStudy/src/ch4/FlowEx12.java
@@ -0,0 +1,18 @@
+package ch4;
+
+public class FlowEx12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1; i<=5; i++)
+			System.out.println(i); //i 값 출력
+		
+		for(int i=1; i<=5; i++)
+			System.out.print(i);
+				
+				System.out.println();
+
+	}
+
+}
