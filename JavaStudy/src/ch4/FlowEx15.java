f452eac25c0a943e69114eea2beb7bc0ce78cc5c
kimue24 <54491294+kimue24@users.noreply.github.com>
2020-12-31 14:29:24 +0900

    201231

---
+++ JavaStudy/src/ch4/FlowEx15.java
@@ -0,0 +1,13 @@
+package ch4;
+
+public class FlowEx15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int j=1; j<=10; j++) {
+			System.out.println("*");
+		}
+		System.out.println();
+	}
+
+}
